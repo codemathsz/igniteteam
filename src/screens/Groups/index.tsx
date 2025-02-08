08a2cebe4173dab6bbea187a52f1b997112ed1b0
codemathsz <mo284891@gmail.com>
2025-02-08 17:31:58 -0300

    feat: component GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,18 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 export default function Groups() {
   return (
     <Container>
-      <Header showBackButton/>
+      <Header/>
       <Highlight
         title='Turmas'
         subtitle='jogue com a sua turma'
       />
+
+      <GroupCard title='Galera do Ignite'/>
     </Container>
   );
 }
