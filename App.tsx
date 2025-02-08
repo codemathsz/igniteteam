8c4c9d4b194f44e00de5272a68c6da9050122ac2
codemathsz <mo284891@gmail.com>
2025-02-08 15:45:15 -0300

    feat: style statusbar

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from "react-native";
 import {ThemeProvider} from "styled-components";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
@@ -15,6 +16,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle={'light-content'} // icons white
+        backgroundColor={'transparent'}
+        translucent //interface start top
+      />
       {fontsLoaded ? <Groups /> : <Loading/>}
     </ThemeProvider>
   );
