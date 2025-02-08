09273caa456657d5ba485172f0f6c918ccaeda2d
codemathsz <mo284891@gmail.com>
2025-02-08 17:19:23 -0300

    feat: Highlight component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
+import { Highlight } from '@components/Highlight';
 
 export default function Groups() {
   return (
     <Container>
       <Header showBackButton/>
+      <Highlight
+        title='Turmas'
+        subtitle='jogue com a sua turma'
+      />
     </Container>
   );
 }
